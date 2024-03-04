0772a216b11048f243e971d40af04a8683a4f85f
dmartens3 <dmartens3@rrc.ca>
2024-03-04 12:39:42 -0600

    Added a script to print the whole database

---
+++ print_db.rb
@@ -0,0 +1,11 @@
+
+require_relative 'config/environment'
+
+products = Product.all
+
+puts "ID\tTitle\t\t\tDescription\t\t\tPrice\tStock Quantity"
+
+# Print each product
+products.each do |product|
+  puts "#{product.id}\t#{product.title}\t#{product.description}\t#{product.price}\t#{product.stock_quantity}"
+end
